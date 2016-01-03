e5b7268e7bbee7bbc1367706a0c178d80fd6c846
winniehell <git@winniehell.de>
2016-01-04 00:54:36 +0100

    add test call to webOPAC

--- server/search.js
+++ server/search.js
@@ -16,6 +16,65 @@ var NUMBERS = [
   {title: 'Nine'}
 ]
 
+function createFilterFor (query) {
+  return function filterFn (number) {
+    return (number.title.toLowerCase().indexOf(query.toLowerCase()) > -1)
+  }
+}
+
+function findTitle (input) {
+  var titlePattern = /&amp;title=(.+?)&amp;/
+  var titleMatch = titlePattern.exec(input)
+  if (titleMatch && titleMatch.length === 2) {
+    return decodeURIComponent(titleMatch[1]).replace(/\+/g, ' ')
+  }
+}
+
+var superagent = Meteor.npmRequire('superagent')
+var rootUrl = Meteor.settings.webOPAC.rootUrl
+
+function findById (id) {
+  var syncFetch = Meteor.wrapAsync(fetchCatalogItem)
+
+  try {
+    return syncFetch()
+  } catch (e) {
+    console.error(e)
+    return null
+  }
+
+  function fetchCatalogItem (callback) {
+    var url = rootUrl + '/start.do?Login=Internet&BaseURL=this&Query=0000=%22' + id + '%22'
+    console.log(url)
+
+    superagent
+      .get(url)
+      .end(function (err, res) {
+        if (err) {
+          return callback(err, null)
+        }
+
+        if (res.status !== 200) {
+          return callback(new Error(res.status), null)
+        }
+
+        if (res.text.indexOf('noHitsText.jsp') > -1) {
+          return callback(new Error('Not found!'), null)
+        }
+
+        if (res.text.indexOf('<!-- START jsp/result/hit.jsp -->') > -1) {
+          var catalogItem = {
+            title: findTitle(res.text)
+          }
+
+          return callback(null, catalogItem)
+        }
+
+        return callback(new Error('Could not parse response!'), null)
+      })
+  }
+}
+
 function findNumber (query) {
   var results
 
@@ -30,8 +89,9 @@ function findNumber (query) {
   return results
 }
 
-function createFilterFor (query) {
-  return function filterFn (number) {
-    return (number.title.toLowerCase().indexOf(query.toLowerCase()) > -1)
+Meteor.startup(function () {
+  var catalogItem = findById(362191)
+  if (catalogItem) {
+    console.log(catalogItem.title)
   }
-}
+})
